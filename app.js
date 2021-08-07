2b54f22972fd32fd0072b781dae83f7b8b2b4a9f
bill-hennessey <bhennessey1020@gmail.com>
2021-08-06 22:38:00 -0600

    more JS tweaking

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("here we go...");
-var scheduleHour;
+
 // place current date at the top of the page
 $("#currentDay").text(moment().format("dddd, MMM Do"));
 
@@ -9,31 +9,34 @@ var time = moment().toArray();
 // gather all rows and make them an array
 var rows = $(".row").toArray();
 // gather all columns with textareas in to an array
+
+console.log($(".hour"));
 var allHours = $(".hour").toArray();
+console.log(allHours);
 
 // add an event handler to the icons to save input data to local storage
 $("i").on("click", function (event) {
   event.preventDefault();
+  var input = document.querySelector("textarea");
 
-  var task = $("textarea").val();
+  var task = $("input").val();
   console.log(task);
-  localStorage.setItem(scheduleHour, task);
-  console.log(scheduleHour);
+  localStorage.setItem("task", task);
+  // console.log(scheduleHour);
 });
 
 // event handler to add styling to rows based on current hour
-window.onload = function () {
-  for (var i = 0; i < rows.length; i++) {
-    var scheduleHour = allHours[i].id;
 
-    var actualHour = time[3].toString();
+for (var i = 0; i < rows.length; i++) {
+  var scheduleHour = allHours[i].id;
 
-    if (scheduleHour === actualHour) {
-      $(".hour").addClass("present");
-    } else if (scheduleHour > actualHour) {
-      $(".hour").addClass("future");
-    } else {
-      $(".hour").addClass("past");
-    }
+  var actualHour = time[3].toString();
+
+  if (scheduleHour === actualHour) {
+    $(".hour").addClass("present");
+  } else if (scheduleHour > actualHour) {
+    $(".hour").addClass("future");
+  } else {
+    $(".hour").addClass("past");
   }
-};
+}
