a7ebed2468ba7a176ca45bcefba4b2dc203d76de
bill-hennessey <bhennessey1020@gmail.com>
2021-08-06 21:52:33 -0600

    edits to html, css and JS.

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
 console.log("here we go...");
-
+var scheduleHour;
+// place current date at the top of the page
 $("#currentDay").text(moment().format("dddd, MMM Do"));
 
-// create text inputs for the rows, or maybe in the rows?
+// take current time and make it an array
+var time = moment().toArray();
 
-// set a var for the current time
-var time = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
-console.log(time);
+// gather all rows and make them an array
+var rows = $(".row").toArray();
+// gather all columns with textareas in to an array
+var allHours = $(".hour").toArray();
 
-// https://momentjs.com/docs/#/query/is-before/
+// add an event handler to the icons to save input data to local storage
+$("i").on("click", function (event) {
+  event.preventDefault();
 
-var foo = moment([2021, 7, 5, 23, 0]);
+  var task = $("textarea").val();
+  console.log(task);
+  localStorage.setItem(scheduleHour, task);
+  console.log(scheduleHour);
+});
 
-if (moment().isBefore(foo)) {
-  console.log("true");
-}
+// event handler to add styling to rows based on current hour
+window.onload = function () {
+  for (var i = 0; i < rows.length; i++) {
+    var scheduleHour = allHours[i].id;
+
+    var actualHour = time[3].toString();
+
+    if (scheduleHour === actualHour) {
+      $(".hour").addClass("present");
+    } else if (scheduleHour > actualHour) {
+      $(".hour").addClass("future");
+    } else {
+      $(".hour").addClass("past");
+    }
+  }
+};
